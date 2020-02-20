dc6a46a8316711ed36a1f5a3159019709b00496c
BhartiGarde <bhartigarde@gmail.com>
2020-02-20 08:51:10 +0530

    Fixed delete button and added search bar

--- k8s-frontend/src/components/PodsPage/PodCard.js
+++ k8s-frontend/src/components/PodsPage/PodCard.js
@@ -1,37 +1,146 @@
 import React, { Component } from "react";
-import { deletePod } from "../../services";
+import { deletePod, getPodExposure } from "../../services";
 import { CardLabels, CardContainerList } from "../common";
+import { BounceLoader } from "react-spinners";
+import ReactTooltip from "react-tooltip";
 
 class PodCard extends Component {
+	state = { exposures: [] };
+	_isMounted = false;
+
+	constructor(props) {
+		super(props);
+
+		getPodExposure(this.props.namespace, this.props.podInfo.podName)
+			.then(result => {
+				this.setState({
+					...this.state,
+					exposures: result.payLoad
+				});
+			})
+			.catch(err => {
+				this.setState({ ...this.state, exposures: [] });
+			});
+	}
+
+	componentDidMount() {
+		this._isMounted = true;
+	}
+
+	componentWillUnmount() {
+		this._isMounted = false;
+	}
+
+	deletePodX(namespace, podName) {
+		deletePod(namespace, podName);
+		this.props.deleteCard(podName);
+	}
+
 	render() {
 		return (
 			<React.Fragment>
 				<div className="card flex flex-column">
-					<span className="w-100">
-						<span className="title">
+					{/* Card Header */}
+					<span className="flex flex-row space-between w-100 ">
+						{/* Card Title + Live Status */}
+						<span className="flex flex-row title mw-80">
 							<span className="fa fa-lastfm" />
 							&emsp;
-							{this.props.podInfo.podName}
+							<span>{this.props.podInfo.podName}</span>
+							{this.state.exposures &&
+								this.state.exposures.length !== 0 && (
+									<span className="live-icon">
+										<a
+											href="#deletePod"
+											data-tip
+											data-for={
+												"reacttip" + this.props.index
+											}
+										>
+											<BounceLoader
+												id="live"
+												size={"0.50em"}
+												color={"red"}
+											/>
+										</a>
+
+										<ReactTooltip
+											id={"reacttip" + this.props.index}
+											type="dark"
+											place="bottom"
+											effect="float"
+										>
+											<span>
+												{this.state.exposures &&
+													this.state.exposures.map(
+														(object, index) => {
+															return (
+																<React.Fragment
+																	key={
+																		index +
+																		"_FRAG"
+																	}
+																>
+																	<span className="tooltip-highlight">
+																		Exposed
+																		as:
+																	</span>
+																	<br />
+																	{
+																		object.serviceName
+																	}
+																	{":"}
+																	&nbsp;
+																	{
+																		object.port
+																	}{" "}
+																	&nbsp;
+																	<span className="fa fa-arrow-circle-right" />
+																	&nbsp;{" "}
+																	{
+																		object.targetPort
+																	}{" "}
+																	over{" "}
+																	<span className="tooltip-highlight">
+																		{
+																			object.serviceType
+																		}
+																	</span>
+																	<hr />{" "}
+																</React.Fragment>
+															);
+														}
+													)}
+											</span>
+										</ReactTooltip>
+									</span>
+								)}
 							{this.props.showResourceType && (
 								<sup className="resource-type">Pod</sup>
 							)}
 						</span>
 
+						{/* Card Resource Manip Button */}
 						<span className="resource-manage-section">
-							<span
-								className="resource-delete-button fa fa-trash"
-								onClick={() =>
-									deletePod(
-										this.props.namespace,
-										this.props.podInfo.podName
-									)
-								}
-							/>
+							{this.props.podInfo.podStatus === "Running" && (
+								<span
+									className="resource-delete-button fa fa-trash"
+									onClick={() => {
+										if (this._isMounted)
+											this.deletePodX(
+												this.props.namespace,
+												this.props.podInfo.podName
+											);
+									}}
+								/>
+							)}
+
 							<span className="resource-restart-button fa fa-refresh" />
 							<span className="resource-terminal-button fa fa-terminal" />
 						</span>
 					</span>
 
+					{/* Card Body */}
 					<br />
 					<span className="left-orange-border">
 						<CardContainerList
